a5a440c2ba734de6b6c70e5e3958f3ac686c1e17
성인 김 <ksi132@naver.com>
2023-03-14 02:17:46 +0900

    {2022.03.14} 1. @NoArgsConstructor 추가

--- src/main/java/com/example/demo/src/domain/match/dto/ByNetworkRes.java
+++ src/main/java/com/example/demo/src/domain/match/dto/ByNetworkRes.java
@@ -2,11 +2,13 @@ package com.example.demo.src.domain.match.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ByNetworkRes {
     private String date; // 경기 날짜
     private int average; // 점수대
