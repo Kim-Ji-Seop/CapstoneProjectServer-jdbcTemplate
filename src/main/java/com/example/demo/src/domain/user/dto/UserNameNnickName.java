f5361a91b0b8c0d1924181b9b8c62c26c82d2d02
성인 김 <ksi132@naver.com>
2023-03-15 04:03:11 +0900

    {2022.03.15} 1. 서버 빌드 실패 후 재 커밋

---
+++ src/main/java/com/example/demo/src/domain/user/dto/UserNameNnickName.java
@@ -0,0 +1,15 @@
+package com.example.demo.src.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserNameNnickName {
+    private String name;
+    private String nickname;
+}
--- src/main/java/com/example/demo/src/domain/user/dto/UserSimpleInfo.java
+++ src/main/java/com/example/demo/src/domain/user/dto/UserSimpleInfo.java
@@ -10,8 +10,12 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserSimpleInfo {
-    private String name;
-    private String nickname;
-
+    private String name; // 이름
+    private String nickname; // 닉네임
+    private int average; // 점수 평균
+    // 최근 10 경기 승, 패
+    private int win_count;
+    private int lose_count;
+    private int win_late;
 
 }
