ab4fedc0e775beab7165d1654907a3a40d1843bc
성인 김 <ksi132@naver.com>
2023-03-22 22:20:48 +0900

    {2022.03.20} 1. 소켓 통신 테스트 3 매칭방 생성 후 ID를 받아서 사용하는 방식으로 변경

--- src/main/java/com/example/demo/src/domain/game/websock/MatchCodeController.java
+++ src/main/java/com/example/demo/src/domain/game/websock/MatchCodeController.java
@@ -1,15 +1,37 @@
 package com.example.demo.src.domain.game.websock;
 
 
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.src.domain.game.dto.ChatRoomDTO;
+import com.example.demo.src.domain.game.dto.NewMatchOpenRes;
+import com.example.demo.src.domain.game.service.MatchCodeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping( "/app/game")
 public class MatchCodeController {
+    @Autowired
+    private MatchCodeService matchCodeService;
 
-    /*@GetMapping("/open/{matchCode}")
-    public BaseResponse<NewMatchOpenRes> openNewMatch();*/
+
+    public MatchCodeController(MatchCodeService matchCodeService) {
+        this.matchCodeService = matchCodeService;
+    }
+
+    @PostMapping ("/open")
+    public BaseResponse<ChatRoomDTO> openNewMatch(@RequestParam String id){
+        try{
+            return new BaseResponse<>(matchCodeService.openNewMatch(id));
+        } catch (BaseException exception){
+            return new BaseResponse<>(exception.getStatus());
+        }
+    }
 }
