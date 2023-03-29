fc13e2aa9ca85e8ed87f6b4a5f9b289564ba79fa
성인 김 <ksi132@naver.com>
2023-03-30 03:46:55 +0900

    {2022.03.30} sockJs 미사용

--- src/main/java/com/example/demo/src/domain/game/websock/StompWebSocketConfig.java
+++ src/main/java/com/example/demo/src/domain/game/websock/StompWebSocketConfig.java
@@ -17,8 +17,7 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp/game")
-                .setAllowedOrigins("https://www.seop.site")
-                .withSockJS();
+                .setAllowedOrigins("https://www.seop.site");
     }
 
     /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
