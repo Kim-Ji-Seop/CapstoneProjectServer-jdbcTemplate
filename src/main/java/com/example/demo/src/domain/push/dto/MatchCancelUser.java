2810237faa549eae1d1c64b02d87ae06ff5309c4
Kim-Ji-Seop <76149358+Kim-Ji-Seop@users.noreply.github.com>
2023-04-13 12:22:48 +0900

    매칭방취소 API 푸시알림 진행중

--- src/main/java/com/example/demo/src/domain/push/dto/MatchCancelUser.java
+++ src/main/java/com/example/demo/src/domain/push/dto/MatchCancelUser.java
@@ -5,13 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class MatchCancelReq {
-    private int matchIdx;
-    private List<MatchCancelUser> matchCancelUserList;
+public class MatchCancelUser {
+    private int userIdx;
 }
