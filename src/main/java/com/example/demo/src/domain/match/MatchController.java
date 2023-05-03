984697a6a832b3306a0f8a6eb6ffae2a17c2c0a1
성인 김 <ksi132@naver.com>
2023-05-03 23:51:59 +0900

    23.05.03 오프라인 매칭 디버깅

--- src/main/java/com/example/demo/src/domain/match/MatchController.java
+++ src/main/java/com/example/demo/src/domain/match/MatchController.java
@@ -71,6 +71,7 @@ public class MatchController {
                                                                    @RequestParam(required = false) String localName,
                                                                    @RequestParam(required = false) String cityName){
         try {
+            System.out.println(network + " " + localName + " " + cityName + " ");
             List<ByNetworkRes> byNetworkRes = matchService.getMatchRoomsByNetwork(network, localName, cityName);
             return new BaseResponse<>(byNetworkRes);
         }catch (BaseException baseException){
