dde5ab0d09ebe38a9b75acb080f5d24dc8fa38b5
성인 김 <ksi132@naver.com>
2023-05-14 00:12:33 +0900

    23.05.13 심플인포 승률 수정

--- src/main/java/com/example/demo/src/domain/user/service/UserService.java
+++ src/main/java/com/example/demo/src/domain/user/service/UserService.java
@@ -125,7 +125,13 @@ public class UserService {
             int recentWinCount = historyDao.getRecentWinCount(userIdx);
             int recentLoseCount = historyDao.getRecentLoseCount(userIdx);
             int recentDrawCount = historyDao.getRecentDrawCount(userIdx);
-            int recentWinRate = recentWinCount / 10;
+            int maxTenGame = 10;
+
+            if (recentWinCount + recentLoseCount + recentDrawCount < 10){
+                maxTenGame = recentWinCount + recentLoseCount + recentDrawCount;
+            }
+
+            int recentWinRate = recentWinCount / maxTenGame;
 
             return new UserSimpleInfo(
                     userInfo.getName(),
