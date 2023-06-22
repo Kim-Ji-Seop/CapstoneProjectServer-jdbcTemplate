db35184bb3a04b67b7a9a3455c50079ecfde30cc
Kim-Ji-Seop <76149358+Kim-Ji-Seop@users.noreply.github.com>
2023-06-22 19:11:53 +0900

    클래스 이름 수정

--- src/main/java/com/example/demo/src/domain/game/dto/ScoreSendMessageDTO.java
+++ src/main/java/com/example/demo/src/domain/game/dto/ScoreSendMessageDTO.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class ChatMessageDTO {
+public class ScoreSendMessageDTO {
 
     private String matchIdx;
     private String writer;
--- src/main/java/com/example/demo/src/domain/game/websock/StompGameController.java
+++ src/main/java/com/example/demo/src/domain/game/websock/StompGameController.java
@@ -1,7 +1,7 @@
 package com.example.demo.src.domain.game.websock;
 
 import com.example.demo.src.domain.game.dto.AdminSendScoreDTO;
-import com.example.demo.src.domain.game.dto.ChatMessageDTO;
+import com.example.demo.src.domain.game.dto.ScoreSendMessageDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -17,13 +17,13 @@ public class StompGameController {
     //stompConfig에서 설정한 applicationDestinationPrefixes와 @MessageMapping 경로가 병합됨
     //"/pub/chat/enter"
     @MessageMapping(value = "/game/enter")
-    public void enter(ChatMessageDTO message){
+    public void enter(ScoreSendMessageDTO message){
         message.setMessage(message.getWriter() + " 님이 매칭방에 참여하였습니다.");
         template.convertAndSend("/sub/game/room/" + message.getMatchIdx(), message);
     }
 
     @MessageMapping(value = "/game/message")
-    public void message(ChatMessageDTO message){ // 점수 DTO로 수정해야함
+    public void message(ScoreSendMessageDTO message){ // 점수 DTO로 수정해야함
         System.out.println(message.getMatchIdx() + ": " + message.getWriter() + " -> " + message.getMessage());
         template.convertAndSend("/sub/game/room/" + message.getMatchIdx(), message);
     }
